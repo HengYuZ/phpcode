8223eac107a7d96ac560e380424663dab093f4ba
Kimxq <1448768191@qq.com>
2016-07-27 11:32:27 +0800

    nowdocs跟heredocs的区别测试

---
+++ NowDoc跟HereDoc.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhanghengyu
+ * Date: 2016/7/27
+ * Time: 11:23
+ *
+ * 测试nowdocs跟heredocs的区别
+ * nowdocs不能解析变量
+ * 而heredocs 可以解析变量
+ */
+
+$a = 'I am $a';
+$str1 = <<<'EOD'
+hello world
+    I am nowdocs, 不能解析$a;
+EOD;
+
+$str2 = <<<EOD
+hello world
+    I am heredocs,不同于$str1;
+EOD;
+
+var_dump($str1);
+var_dump($str2);
