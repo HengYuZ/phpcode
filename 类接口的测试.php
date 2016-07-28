ef4f8c191b3eab2e22fcd52693f88ff6ff5c78e6
HengYuZ <HengYuZ@users.noreply.github.com>
2016-07-28 09:31:44 +0800

    类接口的一些测试

---
+++ 类接口的测试.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: zhanghengyu
+ * Date: 2016/7/28
+ * Time: 9:48
+ * 类接口测试
+ */
+interface a
+{
+    public function printMyItem();
+
+    const value = 'I am a value';
+}
+
+//接口b继承接口b
+interface b extends a
+{
+    public function bfunction();
+}
+
+
+//类实现接口a,b
+class Myclass implements a, b
+{
+    //报错，不能覆盖接口的常量。
+    //const value = 'I am a value too';
+
+    public function printMyItem()
+    {
+        // TODO: Implement printMyItem() method.
+        echo "hello, a interface's function ";
+    }
+
+    public function bfunction()
+    {
+        // TODO: Implement bfunction() method.
+        echo "hello, b interface's function ";
+    }
+
+    /**
+    PHP不能重载，因为
+     *
+     *1.它能接收多余的参数，
+     * 2.它是弱类型
+     * 所以，php中的函数重载就不可以了
+    public function bfunction($str){
+
+    }
+     */
+
+}
+
+//输出接口变量，下面的是B接口继承A接口的变量value.
+echo 'b接口中value:'.b::value.'<br/>';
+echo 'a接口中value:'.b::value;
